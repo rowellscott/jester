27bbfdc2c38712086824b24ff3b2c504cc05598e
Scott Rowell <rowell.scott@gmail.com>
2018-02-28 08:49:15 -0500

    Before Layout

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport')
 const {ensureLoggedIn, ensureLoggedOut} = require('connect-ensure-login')
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup', ensureLoggedOut('/'), passport.authenticate('local-signup', {
   successRedirect: '/jokes',
   failureRedirect: '/',
   failureFlash: true,
@@ -14,7 +14,7 @@ router.get('/login', (req, res)=>{
    res.render('authentication/login', {layout: 'layouts/main-layout', message: req.flash('error')});
 });
 
-router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
+router.post('/login', ensureLoggedOut('/login'), passport.authenticate('local-login', {
   successRedirect: "/jokes",
   failureRedirect: '/login',
   failureFlash: true, 
